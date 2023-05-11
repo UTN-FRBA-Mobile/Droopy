f22ef3cc01f48d32e23f454e2ac7b6388b5e5ecb
Lucas Cheistwer <lucas.cheistwer@zerf.tech>
2023-05-11 20:46:20 -0300

    add login button and 4s delay

--- app/src/main/java/com/example/droopy/MainActivity.kt
+++ app/src/main/java/com/example/droopy/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.droopy.ui.login.ui.LoginScreen
+import com.example.droopy.ui.login.ui.LoginViewModel
 import com.example.droopy.ui.theme.DroopyTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    LoginScreen()
+                    LoginScreen(LoginViewModel())
                 }
             }
         }
